b2b7577db96cc5fa6478383e376deaa1dae679b7
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-03-15 23:35:58 +0200

    Add files via upload

---
+++ assets/preload/data/fresher/script song name.lua
@@ -0,0 +1,50 @@
+function onCreate()
+    local toughness = checkDifficulty()
+
+	makeLuaSprite('bgThing', 'songs/song-Fresher', -500, -50)
+    scaleObject('bgThing', 1, 1)
+    setObjectCamera('bgThing', 'hud')
+    addLuaSprite('bgThing', true)
+    setScrollFactor('bgThing', 0, 0)
+    setProperty('bgThing.alpha', tonumber(1))
+
+end
+
+function onCreatePost()
+    doTweenX('bgThingMoveIn', 'bgThing', -50, 0.6, 'linear')
+    doTweenX('bgThingText', 'songText', 70, 0.6, 'linear')  -- might need to mess with these for longer names
+    doTweenX('bgThingTextBleb', 'beforeSongText', 20, 0.6, 'linear')  -- might need to mess with these for longer names
+    runTimer('moveOut', 3.7, 1)
+end
+
+function onUpdate()
+
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+    if tag == 'moveOut' then
+        doTweenX('bgThingLeave', 'bgThing', -700, 0.6, 'linear')
+        doTweenX('bgThingLeaveText', 'songText', -500, 0.6, 'linear')  -- might need to mess with these for longer names
+        doTweenX('bgThingLeavePreText', 'beforeSongText', -400, 0.6, 'linear') -- might need to mess with these for longer names
+    end
+end
+
+function onTweenCompleted(tag)
+    if tag == 'bgThingLeave' then
+        removeLuaSprite('bgThing', true)
+        removeLuaText('songText', true)
+        removeLuaText('beforeSongText', true)
+    end
+end
+
+function checkDifficulty()
+    -- not needed really, but cool
+    if difficulty == 2 then
+        return 'HARD';
+    elseif difficulty == 1 then
+        return 'NORMAL';
+    else
+        return 'EASY';
+    end
+
+end
